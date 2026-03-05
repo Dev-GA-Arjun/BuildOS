8a7e0220b360b5a8781e69c3290cad1e2f870a6b
G A ARJUN <gaarjun00@gmail.com>
2026-03-05 16:43:41 +0530

    Updated: CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -26,7 +26,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://localhost:3000",
+        "https://shipwithbuildos.vercel.app/login",
     ],
     allow_credentials=True,
     allow_methods=["*"],
